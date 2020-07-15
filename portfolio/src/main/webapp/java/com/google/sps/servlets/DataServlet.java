972096b97cd7b3b2ae3ab32ed54a0cfca365ca22
Anshuman <anshumanc007@gmail.com>
2020-07-15 17:55:58 +0530

    TestingServlet

--- portfolio/src/main/webapp/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/webapp/java/com/google/sps/servlets/DataServlet.java
@@ -27,6 +27,7 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    String name = request.getParameter("name-input");
+    response.getWriter().println("<h1>Hello " +name "</h1>");
   }
 }
